f51a8bfae3bb0802052adb2023b3efa35be5d062
adrixom <adrianvalrom@gmail.com>
2019-03-06 18:11:29 +0100

    Añadidas verisones asincronas

---
+++ DecryptFuerzaBruta/automatizacion_V1_vs_Asincrono.sh
@@ -0,0 +1,26 @@
+
+PRIMERO='decryptFuerzaBruta_V1.c'
+SEGUNDO='decryptFuerzaBruta_V1_Asincrono.c'
+P_EJECUTABLE='ejecutable_V1.exe'
+S_EJECUTABLE='ejecutable_V1_Asincrono.exe'
+
+mpicc $PRIMERO -o $P_EJECUTABLE -lcrypt
+
+mpicc $SEGUNDO -o $S_EJECUTABLE -lcrypt
+
+
+N_PROCESOS=(2 3 4 5 6 7)
+
+for i in ${N_PROCESOS[@]}; do
+  mpirun -np ${i} $P_EJECUTABLE
+done
+
+for i in ${N_PROCESOS[@]}; do
+  mpirun -np ${i} $S_EJECUTABLE
+done
+
+#Ó
+#for i in ${N_PROCESOS[@]}; do
+#  mpirun -np ${i} $P_EJECUTABLE
+#  mpirun -np ${i} $S_EJECUTABLE
+#done
